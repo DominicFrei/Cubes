b826fac385d51133fa59668e0613f80cc67f1c48
Dominic Frei <business.dominic.frei@gmail.com>
2020-11-04 05:36:22 +0000

    Prepare for build and upload.

---
+++ Assets/Scripts/StartApplication.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartApplication : MonoBehaviour
+{
+    public void StartApplicationButtonClicked()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
